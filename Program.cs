00a84110a9a8b4ad544e0c2b28176112a5bc834c
Horup <sah@sah.dk>
2020-01-22 20:14:53 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace goal
+{
+    class Program
+    {
+        static void Add(string s)
+        {
+            // add to DB
+            Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+           try
+           {
+               var command = args[0];
+                switch(command)
+                {
+                    case "add":
+                    {
+                        var s = args.TakeLast(args.Length-1).Aggregate((a,b)=>
+                        {
+                            return a + " " + b;
+                        });
+                        
+
+                        Add(s);
+                        break;
+                    }
+                    default:
+                    throw new Exception();
+                }               
+           }
+           catch(Exception)
+           {
+               Console.WriteLine("Input failure!");
+           }
+        }
+    }
+}
+ 
