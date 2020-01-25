66220faa53767f9b84161314218265a0266d8119
Horup <sah@sah.dk>
2020-01-25 11:48:54 +0100

    created Common and Server

---
+++ Goal.Common/GoalEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Goal.Common
+{
+    public class GoalEntry
+    {
+        public int Id { get; set; }
+        public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.Now;
+        public string Description { get; set; } = "";
+
+        public override string ToString()
+        {
+            return string.Format("{0}\t{1}", this.Id, this.Description);
+        }
+    }
+}
