5ff57d4ff8e7bf8e273e1043ab24d3faf1147f4f
horup <sah@sah.dk>
2020-01-31 07:34:34 +0100

    updated cors

--- Goal.Server/Startup.cs
+++ Goal.Server/Startup.cs
@@ -53,7 +53,7 @@ namespace Goal.Server
             }
 
             app.UseRouting();
-            app.UseCors(b=>b.WithOrigins("*"));
+            app.UseCors(b=>b.WithOrigins("*").AllowAnyHeader().AllowAnyMethod());
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
